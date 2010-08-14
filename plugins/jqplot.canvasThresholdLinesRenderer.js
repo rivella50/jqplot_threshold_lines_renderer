2f3d5cee0e0e92fb47c903e050a3a5242e63cace
Valentin Treu <valley@icu.uzh.ch>
2010-08-14 12:43:31 +0200

    Initial import

---
+++ plugins/jqplot.canvasThresholdLinesRenderer.js
@@ -0,0 +1,95 @@
+/**
+ * Copyright (c) 2009 - 2010 Chris Leonello
+ * jqPlot is currently available for use in all personal or commercial projects 
+ * under both the MIT and GPL version 2.0 licenses. This means that you can 
+ * choose the license that best suits your project and use it accordingly. 
+ *
+ * The author would appreciate an email letting him know of any substantial
+ * use of jqPlot.  You can reach the author at: chris dot leonello at gmail 
+ * dot com or see http://www.jqplot.com/info.php .  This is, of course, 
+ * not required.
+ *
+ * If you are feeling kind and generous, consider supporting the project by
+ * making a donation at: http://www.jqplot.com/donate.php .
+ *
+ * Thanks for using jqPlot!
+ * 
+ */
+(function($) {    
+    /**
+     * Class: $.jqplot.CanvasThresholdLinesRenderer
+     * Plugin renderer to draw n horizontal threshold lines.
+		 * 
+     * > seriesDefaults: {
+     * >   thresholdLines: {
+		 * >          color: '#8f8f8f',
+		 * >          yValues: [25.0, 50.0, 75.0, 5.0],
+		 * >          showLabel: true,     -> [true|false]
+		 * >          labelLocation: 'nw'  -> ['nw'|'ne'|'sw'|'se']
+		 * >   }
+     * > }
+     *
+     * Author: Valentin Treu
+     * Last update: v1.0.1  2010-07-20
+     */
+    $.jqplot.CanvasThresholdLinesRenderer = function(options){
+        this.color = '#666666';
+				this.yValues = [];
+				this.showLabel = true;
+				this.labelLocation = 'nw'; 
+        this.font  = '8pt Arial';
+    };
+
+    $.jqplot.postSeriesInitHooks.push(parseOptions);
+    $.jqplot.preDrawSeriesHooks.push(drawLines);
+
+    function parseOptions (seriesDefaults, options) {
+	
+				this.thresholdRenderer = new $.jqplot.CanvasThresholdLinesRenderer();
+        $.extend(true, this.thresholdRenderer, seriesDefaults.thresholdLines, this.thresholdLines);
+    }
+    
+    function drawLines(sctx, options) {
+	
+        options = $.extend(true, {}, this.thresholdRenderer, options);
+				var inputData = [];
+        for (var i=0; i < options.yValues.length; i++) {
+					inputData.push([0, options.yValues[i]]);
+				}
+				var gridData = this.renderer.makeGridData.call(this, inputData);
+        sctx.save();
+        
+				for (var i=0; i < inputData.length; i++) {
+					sctx.moveTo(0, gridData[i][1]);
+					sctx.lineTo(sctx.canvas.width, gridData[i][1]);
+					if (options.showLabel == true) {
+						var labelCoordinates = determineLabelCoordinates(sctx, options.labelLocation, options.yValues[i]);
+						sctx.font = options.font;
+						sctx.fillStyle = options.color;
+						sctx.fillText(options.yValues[i], labelCoordinates[0], gridData[i][1] + labelCoordinates[1]);
+					}
+					
+				}
+        sctx.strokeStyle = options.color;
+				sctx.stroke();
+        
+				sctx.restore();
+    }
+
+    function determineLabelCoordinates(context, location, string) {
+			var result = [];
+			var width = context.measureText(string).width;
+			if (location == 'nw' || location == 'sw') {
+				result.push(3);
+			} else {
+				result.push(context.canvas.width - width - 5);
+			}
+			if (location == 'nw' || location == 'ne') {
+				result.push(-5);
+			} else {
+				result.push(12);
+			}
+			return result;
+	  }
+    
+})(jQuery);
