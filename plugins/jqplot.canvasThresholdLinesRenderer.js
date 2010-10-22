ede345ee0088ba53ade46452189b01be5203394d
Valentin Treu <valley@icu.uzh.ch>
2010-10-22 15:15:29 +0200

    added support for vertical lines, split color parameter in 2 new parameters (lineColor and labelColor)

--- plugins/jqplot.canvasThresholdLinesRenderer.js
+++ plugins/jqplot.canvasThresholdLinesRenderer.js
@@ -22,7 +22,9 @@
 		 * 
      * > seriesDefaults: {
      * >   thresholdLines: {
-		 * >          color: '#8f8f8f',
+		 * >          lineColor: '#8f8f8f',
+		 * >					labelColor: '#454545',	
+		 * >          xValues: [33.0, 78.0, 105.0, 155.0],
 		 * >          yValues: [25.0, 50.0, 75.0, 5.0],
 		 * >          showLabel: true,     -> [true|false]
 		 * >          labelLocation: 'nw'  -> ['nw'|'ne'|'sw'|'se']
@@ -30,10 +32,12 @@
      * > }
      *
      * Author: Valentin Treu
-     * Last update: v1.0.1  2010-07-20
+     * Last update: v1.0.2  2010-10-22
      */
     $.jqplot.CanvasThresholdLinesRenderer = function(options){
-        this.color = '#666666';
+				this.lineColor  = '#333333';
+        this.labelColor = '#666666';
+				this.xValues = [];
 				this.yValues = [];
 				this.showLabel = true;
 				this.labelLocation = 'nw'; 
@@ -52,31 +56,53 @@
     function drawLines(sctx, options) {
 	
         options = $.extend(true, {}, this.thresholdRenderer, options);
-				var inputData = [];
-        for (var i=0; i < options.yValues.length; i++) {
-					inputData.push([0, options.yValues[i]]);
+				var inputDataX = [];
+				var inputDataY = [];
+				for (var i=0; i < options.xValues.length; i++) {
+					inputDataX.push([options.xValues[i], 0]);
 				}
-				var gridData = this.renderer.makeGridData.call(this, inputData);
+        for (var i=0; i < options.yValues.length; i++) {
+					inputDataY.push([0, options.yValues[i]]);
+				}
+				var gridDataX = this.renderer.makeGridData.call(this, inputDataX);
+				var gridDataY = this.renderer.makeGridData.call(this, inputDataY);
         sctx.save();
-        
-				for (var i=0; i < inputData.length; i++) {
-					sctx.moveTo(0, gridData[i][1]);
-					sctx.lineTo(sctx.canvas.width, gridData[i][1]);
+				
+				
+				for (var i=0; i < inputDataY.length; i++) {
+					sctx.beginPath();
+					sctx.moveTo(0, gridDataY[i][1]);
+					sctx.lineTo(sctx.canvas.width, gridDataY[i][1]);
+					sctx.strokeStyle = options.lineColor;
+					sctx.stroke();
 					if (options.showLabel == true) {
-						var labelCoordinates = determineLabelCoordinates(sctx, options.labelLocation, options.yValues[i]);
+						var labelCoordinates = determineLabelCoordinatesHorizontal(sctx, options.labelLocation, options.yValues[i]);
 						sctx.font = options.font;
-						sctx.fillStyle = options.color;
-						sctx.fillText(options.yValues[i], labelCoordinates[0], gridData[i][1] + labelCoordinates[1]);
+						sctx.fillStyle = options.labelColor;
+						sctx.fillText(options.yValues[i], labelCoordinates[0], gridDataY[i][1] + labelCoordinates[1]);
 					}
 					
 				}
-        sctx.strokeStyle = options.color;
-				sctx.stroke();
+				for (var i=0; i < inputDataX.length; i++) {
+					sctx.beginPath();
+					sctx.moveTo(gridDataX[i][0], 0);
+					sctx.lineTo(gridDataX[i][0], sctx.canvas.height);
+					sctx.strokeStyle = options.lineColor;
+					sctx.stroke();
+					if (options.showLabel == true) {
+						var labelCoordinates = determineLabelCoordinatesVertical(sctx, options.labelLocation, options.xValues[i]);
+						sctx.font = options.font;
+						sctx.fillStyle = options.labelColor;
+						sctx.fillText(options.xValues[i], gridDataX[i][0] + labelCoordinates[0], labelCoordinates[1]);
+					}
+					
+				}
         
 				sctx.restore();
     }
-
-    function determineLabelCoordinates(context, location, string) {
+		
+		// this is for label placement of horizontal lines
+    function determineLabelCoordinatesHorizontal(context, location, string) {
 			var result = [];
 			var width = context.measureText(string).width;
 			if (location == 'nw' || location == 'sw') {
@@ -88,6 +114,23 @@
 				result.push(-5);
 			} else {
 				result.push(12);
+			}
+			return result;
+	  }
+		
+		// this is for label placement of vertical lines
+		function determineLabelCoordinatesVertical(context, location, string) {
+			var result = [];
+			var width = context.measureText(string).width;
+			if (location == 'nw' || location == 'sw') {
+				result.push(-(width+3));
+			} else {
+				result.push(3);
+			}
+			if (location == 'nw' || location == 'ne') {
+				result.push(12);
+			} else {
+				result.push(context.canvas.height - 3);
 			}
 			return result;
 	  }
